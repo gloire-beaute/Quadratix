54fc58bcdb057b89821b2f41f23d9c00f5b0e288
Valentin Berger <Valentin.berger38@gmail.com>
2019-04-02 17:39:15 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quadratix #
+
+Quadratix is a student project that assigns a place to a set of machines using quadratic assignement methods.
---
+++ quadratix.r
@@ -0,0 +1,5 @@
+# Title     : Quadratix
+# Objective : Quadratix is a student project that assigns a place to a set of machines using quadratic assignement methods.
+# Created by: Lea CHEMOUL & Valentin BERGER
+# Created on: 01/04/2019
+
